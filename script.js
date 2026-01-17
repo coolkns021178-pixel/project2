849c109ce74652a53844692e00c2351311aed8ca
Nata <nata-sk78@mail.ru>
2026-01-17 18:34:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+let a = 10;
+console.log(a);
+// let persone ={
+//     name :'Djon',
+//     age :25,
+//     isMarried: false
+// };
+// console.log(persone['name']);
+
+// let arr =['plum.png', 'oranje.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+//alert('Hello');
+
+//let answer =confirm('Вы здесь?');
+//console.log(answer);
+
+// let answer = +prompt('Вам есть уже 18 лет?', 'да');
+// console.log(typeof(answer));
+// console.log('arr'+' - oject');
+// console.log(4+' - oject');
+let incr = 10,
+    descr = 10;
+
+    
+    console.log(incr++);
+    console.log(descr--);
+    console.log(5%2);
+    console.log('2'==2);
+
+    let isChecked = true,
+        isClose = false;
+    console.log(isChecked || !isClose);    
